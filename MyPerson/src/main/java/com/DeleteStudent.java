d0c77719761e86a1445525adf135174643b41551
skrrakesh <126138982+skrrakesh@users.noreply.github.com>
2023-03-03 22:43:22 +0530

    Student

---
+++ MyPerson/src/main/java/com/DeleteStudent.java
@@ -0,0 +1,25 @@
+package com;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class DeleteStudent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("vikas");
+		EntityManager entityManager=entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		
+		Student student=entityManager.find(Student.class,103);
+		entityTransaction.begin();
+		
+		entityManager.remove(student);
+		
+		entityTransaction.commit();
+
+	}
+
+}
